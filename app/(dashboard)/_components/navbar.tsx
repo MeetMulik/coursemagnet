3dd260fdbfe779326b2bf50b5b7807ea73445106
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-09-21 23:30:55 +0530

    Layout Updates✨

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import MobileSidebar from "./mobile-sidebar"
 
 const Navbar = () => {
     return (
-        <div className="p-4 border-b h-full items-center bg-white shadow-sm">
+        <div className=" flex p-4 border-b h-full items-center bg-white shadow-sm">
             <MobileSidebar />
             <NavbarRoutes />
         </div>
